771e9dc2bfb229420fba20e8b01682e771ef68dc
Pabl0Parra <pabsblue16@gmail.com>
2023-01-01 07:46:35 +0100

    created Logo.tsx, Header.tsx, Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+
+export default function Footer() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: 150,
+        bgcolor: "inherit",
+        px: "60px",
+      }}
+    >
+      <Box>
+        <Divider>
+          <Typography color="grey.700" variant="h6">
+            Developed by{" "}
+            <Link
+              href="https://www.linkedin.com/in/pablo-parra-bcn"
+              underline="none"
+              sx={{ color: "text.primary" }}
+              target="_blank"
+            >
+              Pabl0Parra
+            </Link>
+          </Typography>
+        </Divider>
+      </Box>
+    </Box>
+  );
+}
